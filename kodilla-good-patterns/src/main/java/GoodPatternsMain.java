d96e7696529d66d8fba196317a730ead4c4d04cb
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-02-08 15:09:18 +0100

    13.1 Added good-patterns module + task: stream of movie titles

---
+++ kodilla-good-patterns/src/main/java/GoodPatternsMain.java
@@ -0,0 +1,19 @@
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.stream.Collectors;
+
+public class GoodPatternsMain {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String listToPrint = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(listToPrint);
+    }
+}
+
+

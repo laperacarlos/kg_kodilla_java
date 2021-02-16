f61e589a4ba4819dcc106afef95b1f414a5e6e5d
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-02-16 16:40:01 +0100

    14.2 Added training module - package com.kodilla.spring.library

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,6 +1,5 @@
-package com.kodilla;
+package com.kodilla.spring.forum;
 
-import com.kodilla.spring.forum.ForumUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;

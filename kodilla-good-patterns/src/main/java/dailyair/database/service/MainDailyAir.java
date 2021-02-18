5f743572c80212753dc0658b690d28cfed802718
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-02-18 16:45:36 +0100

    13.5 Removed InfromationAfterSearch class

--- kodilla-good-patterns/src/main/java/dailyair/database/service/MainDailyAir.java
+++ kodilla-good-patterns/src/main/java/dailyair/database/service/MainDailyAir.java
@@ -5,13 +5,12 @@ public class MainDailyAir {
     public static void main(String[] args) {
         FlightDatabaseRetriever flightDatabaseRetriever = new FlightDatabaseRetriever();
         FlightDatabase testDataBase = flightDatabaseRetriever.retriever();
-        InformationAfterSearch informationAfterSearch = new InformationAfterSearch();
 
         DepartureAirportSearchRequest departureAirportSearchRequest = new DepartureAirportSearchRequest("Wroclaw");
         ArrivalAirportSearchRequest arrivalAirportSearchRequest = new ArrivalAirportSearchRequest("Warszawa");
         GoingThroughSearchRequest goingThroughRequest = new GoingThroughSearchRequest("Gdansk", "Warszawa", true, "Wroclaw");
 
-        SearchService searchService = new SearchService(testDataBase, informationAfterSearch);
+        SearchService searchService = new SearchService(testDataBase);
 
         searchService.process(departureAirportSearchRequest);
         searchService.process(arrivalAirportSearchRequest);
--- kodilla-good-patterns/src/main/java/dailyair/database/service/SearchService.java
+++ kodilla-good-patterns/src/main/java/dailyair/database/service/SearchService.java
@@ -7,11 +7,9 @@ import java.util.stream.Collectors;
 public class SearchService {
 
     private final FlightDatabase flightDatabase;
-    private final InformationAfterSearch informationAfterSearch;
 
-    public SearchService(FlightDatabase flightDatabase, InformationAfterSearch informationAfterSearch) {
+    public SearchService(FlightDatabase flightDatabase) {
         this.flightDatabase = flightDatabase;
-        this.informationAfterSearch = informationAfterSearch;
     }
 
     public Set<Flight> process(SearchRequest searchRequest) {
@@ -23,23 +21,71 @@ public class SearchService {
                         .filter(flight -> Objects.equals(flight.getDepartureAirport(), searchRequest.getRequestedDepartureAirport()))
                         .collect(Collectors.toSet());
 
-                informationAfterSearch.departureAirportInformation(searchRequest, processedFlights);
+                if (processedFlights.isEmpty()) {
+                    System.out.println("\nThere are no flights from the selected airport.");
+                } else {
+                    System.out.println("\nList of flights from: " + searchRequest.getRequestedDepartureAirport());
+                    for (Flight flight : processedFlights) {
+                        System.out.println(flight);
+                    }
+                }
             } else {
                 processedFlights = flightDatabase.getFlightDatabase().stream()
                         .filter(flight -> flight.getArrivalAirport().equals(searchRequest.getRequestedArrivalAirport()))
                         .collect(Collectors.toSet());
 
-                informationAfterSearch.arrivalAirportInformation(searchRequest, processedFlights);
+                if (processedFlights.isEmpty()) {
+                    System.out.println("\nThere are no flights to the selected airport.");
+                } else {
+                    System.out.println("\nList of flights to: " + searchRequest.getRequestedArrivalAirport());
+                    for (Flight flight :processedFlights) {
+                        System.out.println(flight);
+                    }
+                }
             }
         } else {
+
             processedFlights = flightDatabase.getFlightDatabase().stream()
                     .filter(flight -> (flight.getDepartureAirport().equals(searchRequest.getRequestedDepartureAirport()) && flight.getArrivalAirport().equals(searchRequest.getRequestedGoingThroughAirport()) ||
                             flight.getDepartureAirport().equals(searchRequest.getRequestedGoingThroughAirport()) && flight.getArrivalAirport().equals(searchRequest.getRequestedArrivalAirport())))
                     .collect(Collectors.toSet());
 
-            informationAfterSearch.goingThroughInformation(searchRequest, processedFlights);
+            if (processedFlights.isEmpty()) {
+                System.out.println("\nThere are no flights covering selected trips.");
+            } else if (goingThroughArrivalAirportCheck(searchRequest, processedFlights)) {
+                System.out.println("\nThere are no flights from: " + searchRequest.getRequestedDepartureAirport() + " to: " + searchRequest.getRequestedGoingThroughAirport());
+                System.out.println("List of flights from: " + searchRequest.getRequestedGoingThroughAirport() + " to: " + searchRequest.getRequestedArrivalAirport() + " below:");
+                for (Flight flight : processedFlights) {
+                    System.out.println(flight);
+                }
+            } else if (goingThroughDepartureAirportCheck(searchRequest, processedFlights)) {
+                System.out.println("\nThere are no flights from: " + searchRequest.getRequestedGoingThroughAirport() + " to: " + searchRequest.getRequestedArrivalAirport());
+                System.out.println("List of flights from: " + searchRequest.getRequestedDepartureAirport() + " to: " + searchRequest.getRequestedGoingThroughAirport() + " below:");
+                for (Flight flight : processedFlights) {
+                    System.out.println(flight);
+                }
+            } else {
+                System.out.println("\nList of flights from: " + searchRequest.getRequestedDepartureAirport() + " to: " + searchRequest.getRequestedArrivalAirport() + " via: " + searchRequest.getRequestedGoingThroughAirport());
+                for (Flight flight : processedFlights) {
+                    System.out.println(flight);
+                }
+            }
         }
         return processedFlights;
+    }
+
+    private boolean goingThroughDepartureAirportCheck(SearchRequest searchRequest, Set<Flight> flightSet) {
+        Set<Flight> setToCheck = flightSet.stream()
+                .filter(flight -> flight.getDepartureAirport().equals(searchRequest.getRequestedGoingThroughAirport()))
+                .collect(Collectors.toSet());
+        return setToCheck.isEmpty();
+    }
+
+    private boolean goingThroughArrivalAirportCheck(SearchRequest searchRequest, Set<Flight> flightSet) {
+        Set<Flight> setToCheck = flightSet.stream()
+                .filter(flight -> flight.getArrivalAirport().equals(searchRequest.getRequestedGoingThroughAirport()))
+                .collect(Collectors.toSet());
+        return setToCheck.isEmpty();
     }
 }
 

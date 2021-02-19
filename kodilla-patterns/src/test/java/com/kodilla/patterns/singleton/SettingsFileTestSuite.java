3e6f562e2478cd6bfcd50612c49432477ff460e1
Karol Gruszczyński <lapera.carlos@gmail.com>
2021-02-19 17:29:52 +0100

    15.1 Training module: enum & Singleton.

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/SettingsFileTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SettingsFileTestSuite {
+    private static SettingsFileEngine settingsFileEngine;
+
+    @BeforeAll
+    public static void openSettingsFile() {
+        settingsFileEngine = SettingsFileEngine.INSTANCE;
+        settingsFileEngine.open("myapp.settings");
+    }
+
+    @AfterAll
+    public static void closeSettingsFile() {
+        settingsFileEngine.close();
+    }
+
+    @Test
+    void getTestFile() {
+        //Given
+        //When
+        String fileName = settingsFileEngine.getFileName();
+        System.out.println("Opened: " + fileName);
+
+        //Then
+        assertEquals("myapp.settings", fileName);
+    }
+
+    @Test
+    void testSaveSettings() {
+        //Given
+        //When
+        boolean result = settingsFileEngine.saveSettings();
+
+        //Then
+        assertTrue(result);
+    }
+
+    @Test
+    void testLoadSettings() {
+        //Given
+        //When
+        boolean result = settingsFileEngine.loadSettings();
+
+        //Then
+        assertTrue(result);
+    }
+
+
+}
